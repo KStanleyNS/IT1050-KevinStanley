89b901ad248337fb71b0c12fe7268f04be6cd925
wdfa <ada>
2017-09-28 12:50:22 -0400

    In class assignment - 9/28

---
+++ InClass2/InClass2/House.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClass2
+{
+    class House
+    {
+        public double numRooms;
+        public int numStories;
+        private string color;
+        private bool lightsOn;
+        private int occupants;
+        
+        public void Display()
+        {
+            Console.WriteLine($"House:\nnumRooms={numRooms}\ncolor={color}\nstories={numStories}\nlightsOn={lightsOn}\noccupants={occupants}");
+        }
+
+        
+        
+
+    }
+}
