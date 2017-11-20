803a1aeb43da2c1e251af89f22e4910dd1732cc7
Kevin <32025666+s01099564@users.noreply.github.com>
2017-11-20 07:06:17 -0500

    Midterm

---
+++ IT1050-Midterm/IT1050-Midterm/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050_Midterm
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		// 1.)
+
+		{
+			while (true)
+			{
+				bool keepLooping = true;
+				while (keepLooping == true)
+				{
+					Console.WriteLine("Loop");
+				}
+			}
+		}
+
+
+
+
+		// 2.)
+
+		{
+			int i = 2;
+
+			while (i < 128)
+			{
+
+				Console.WriteLine("[" + i + "]");
+
+
+				i += 2;
+
+
+			}
+		}
+
+
+
+
+		// 3.)
+
+		{
+
+			for (int i = 49; i >= 1; i--)
+			{
+				Console.Write(i + (i != 1 ? "," : ""));
+			}
+
+			Console.WriteLine();
+		}
+
+
+
+
+		//  4.)
+		{
+			int i = -1;
+			while (i <= 20)
+			{
+				i = i + 2;
+				Console.Write(i + (i != -1 ? "   " : ""));
+			}
+
+			Console.Read();
+
+
+		}
+
+
+
+		// 5.) // Do-While Loop will print a single asterisk. The normal While Loop does not print anything.
+		{
+			int n = 8;
+			int i = 10;
+			while (i < n)
+			{
+
+				Console.WriteLine("*");
+
+
+				i++;
+			}
+		}
+
+
+
+
+
+
+		// 6.)
+
+		{
+
+			bool icyRain = true;
+			bool tornadoWarning = true;
+			string name = "Let's go outside";
+
+			if ((icyRain) & (tornadoWarning))
+			{
+				Console.WriteLine("{0}", name);
+			}
+		}
+
+
+
+
+	}
+}
