a5f2e5651f13c6f447228feba71eac331b806a5a
Kevin <32025666+s01099564@users.noreply.github.com>
2017-12-04 23:18:42 -0500

    Lab 11 finished

---
+++ Lab09/Lab09/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab09
+{
+	class MyMath
+	{
+		static void Main(string[] args)
+		{ }
+			
+		public int operand1;
+		public int operand2;
+		public int result;
+
+		public int Add(int i, int b)
+		{
+			operand1 = i;
+			operand2 = b;
+			result = i + b;
+			return result;
+		}
+
+		public void int Multiply(int i, int b)
+		{
+			return i * b;
+		}
+
+		public void int Divide(int i, int b)
+		{
+			return i / b;
+		}
+
+		public void int Subtract(int i, int b)
+		{
+			return i - b;
+		}
+
+	}
+}
+}
---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+
+
+		//1.)
+
+		//{
+		//	string[] names = { "Al Dente", "Anna Graham", "Earle Bird", "Ginger Rayle", "Iona Ford" };
+		//	{
+		//		Console.WriteLine(names[1]);
+		//		Console.WriteLine(names[4]);
+		//	}
+		//}
+
+
+
+
+
+		//2.)
+
+		//{
+		//	string[] months = new string[12];
+
+		//	months[0] = "January";
+		//	months[1] = "February";
+		//	months[2] = "March";
+		//	months[3] = "April";
+		//	months[4] = "May";
+		//	months[5] = "June";
+		//	months[6] = "July";
+		//	months[7] = "August";
+		//	months[8] = "September";
+		//	months[9] = "October";
+		//	months[10] = "November";
+		//	months[11] = "December";
+
+		//	for (int i = 0; i < months.Length; i++)
+		//	{
+		//		Console.WriteLine(months[i]);
+		//	}
+		//}
+
+
+
+
+
+		//3.)
+
+		//{
+		//	List<string> season = new List<string>();
+		//	season.Add("Spring");
+		//	season.Add("Summer");
+		//	season.Add("Fall");
+		//	season.Add("Winter");
+
+
+
+		//	foreach (string seasons in season)
+		//	{
+		//		Console.WriteLine(seasons);
+		//	}
+
+		//}
+
+		//4.)
+
+		//{ 
+		//Random random = new Random();
+		//int randomNumber
+
+		//randomNumber = Random.Next(0,100); 
+
+		//int[] randomNumber = new int[1000];
+		//Random random = new Random();
+
+		//for (int i = 0; i < randomNumber.Length; i++)
+		//	{
+		//		randomNumber[i] = random.Next(0, 100);
+		//	}
+
+		//foreach (int i in randomNumber)
+		//{
+		//Console.WriteLine(int.ToString());
+		//}
+
+	}
+}
+
+
+	
