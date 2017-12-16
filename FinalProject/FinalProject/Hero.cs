f01c0854651cb0209fb319c37db0a86450384be5
Kevin <32025666+s01099564@users.noreply.github.com>
2017-12-15 21:46:30 -0500

    FinalProject progress

---
+++ FinalProject/FinalProject/Hero.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+	class Hero
+	{
+		private string Alterego;
+		private string Weakness;
+		private bool Antihero;
+		private string Gender;
+		private string Race;
+
+		public string Power;
+		public int PowerLevel;
+		private int increaseAmouunt;
+		public double Health;
+		public string Weapons;
+		public int Age;
+		public string Name;
+		public string nemesis;
+		
+
+		public Hero()
+		{
+		}
+
+		public Hero(string name, string nemesis, string race, string power, int powerlevel)
+		{
+			this.Name = name;
+			this.nemesis = nemesis;
+			this.Race = race;
+			this.Power = power;
+			this.PowerLevel = powerlevel;
+
+		}
+
+		public Hero(string name, string nemesis, string race, string power, bool antihero, int powerlevel)
+		{
+			this.Name = name;
+			this.nemesis = nemesis;
+			this.Race = race;
+			this.Power = power;
+			this.Antihero = antihero;
+			this.PowerLevel = powerlevel;
+			
+		}
+
+		public Hero(string name, string race, string weakness, string power, string gender, int powerlevel)
+		{
+			this.Name = name;
+			this.Race = race;
+			this.Weakness = weakness;
+			this.Power = power;
+			this.Gender = gender;
+			this.PowerLevel = powerlevel;
+		}
+
+
+		public void FightCrime(string address)
+		
+		{
+			// TODO: Problem 2 - Output "Successfully fought crime at address" if PowerLevel > 49
+		}
+
+		private bool Investigate()
+		{
+
+			
+			bool returnValue = true;
+			return returnValue;
+
+			if (returnValue = true)
+			{
+				Console.WriteLine("Investigated Crime");
+			}
+			else
+			{
+				Console.WriteLine("Did not investigate Crime");
+			}
+		}
+
+		public void Fly()
+		{
+			
+			if (this.Power.Contains("Fly"))
+			{
+				Console.WriteLine(Name + "is flying!");
+			}
+
+			else
+			{
+				Console.WriteLine(Name + "can't fly!");
+			}
+		}
+
+		public void Craft(string material, int numMaterials)
+		{
+			Console.WriteLine("Crafting with " + material);
+		}
+
+		public void Climb(int height)
+		{
+			Console.WriteLine(this.Name + "is climbing");
+		}
+
+		public int ChargePower(int powerlevel)
+		{
+			this.PowerLevel =+ 1;
+			return PowerLevel;
+		}
+
+		public int ChargePower(int powerlevel, int increaseamount)
+		{
+			this.PowerLevel = +1;
+			increaseAmouunt = +1;
+			return PowerLevel;
+		}
+
+	}
+}
---
+++ FinalProject/FinalProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Hero myHero = new Hero();
+			Hero myOtherHero = new Hero("Martian Manhunter", "White Martian", "Human", "Speed, Climb, Fly, FightCrime, Investigate", true, 48 );
+			Hero thirdHero = new Hero("Wonder Woman", "All bad people", "Amazonian", "ChargePower, Stength, Speed, Fly, Climb, FightCrime, Investigate", 60);
+			Hero fourthHero = new Hero("Superman", "Kryptonian", "Kryptonite", "ChargePower, Strength, Fly, Speed, Climb, FightCrime, Investigate", "Male", 70 );
+			
+
+      
+		}
+	}
+}
